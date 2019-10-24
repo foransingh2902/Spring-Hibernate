539f218d953e916cce8257712ac8de3c0de3e776
fsingh <fsingh@AARON-DEV20.bebotechnologies.com>
2019-10-24 18:49:26 +0530

    24/10/2019

---
+++ spring-demo-aop-pointcut-declarations-combo/src/com/luv2code/aopdemo/MainDemoApp.java
@@ -0,0 +1,43 @@
+package com.luv2code.aopdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.luv2code.aopdemo.dao.AccountDAO;
+import com.luv2code.aopdemo.dao.MembershipDAO;
+
+public class MainDemoApp {
+
+	public static void main(String[] args) {
+		// read spring config java class
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DemoConfig.class);
+
+		// get the bean from spring container. the default name is decapitalized version
+		// of the original class. We can give our own name.
+		AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+		// get membership bean from spring container
+		MembershipDAO theMembershipDAO = context.getBean("membershipDAO", MembershipDAO.class);
+
+		// call the business method
+		Account myAccount = new Account();
+		theAccountDAO.addAccount(myAccount, true);
+		System.out.println();
+		theAccountDAO.doWork();
+
+		// call the accountDAO getter/setter methods
+		theAccountDAO.setName("foran singh");
+		theAccountDAO.setServiceCode("silver");
+
+		String name = theAccountDAO.getName();
+		String code = theAccountDAO.getServiceCode();
+
+		System.out.println("\n calling it for MembershipDAO");
+		theMembershipDAO.addAccount();
+		System.out.println();
+		theMembershipDAO.goToSleep();
+
+		// close the context
+		context.close();
+
+	}
+
+}
---
+++ spring-demo-aop-pointcut-declarations-combo/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,48 @@
+package com.luv2code.aopdemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class MyDemoLoggingAspect {
+	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.*(..))")
+	private void forDaoPackage() {
+	}
+
+	// create pointcut for getter methods
+	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.get*(..))")
+	private void getter() {
+		// method can have any name
+	}
+
+	// create pointcut for setter methods
+	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.set*(..))")
+	private void setter() {
+		// method can have any name
+	}
+
+	// create pointcut: include package... exclude getter/setter
+	@Pointcut("forDaoPackage() && !(getter() || setter())")
+	private void forDaoPackageNoGetterSetter() {
+		// can have any name for method
+	}
+
+
+	// Apply the pointcut
+
+//	@Before("forDaoPackage()")
+	@Before("forDaoPackageNoGetterSetter()")
+	public void beforeAddAccountAdvice() {
+		System.out.println("\n=======>>>>> execution @Before advice on addAccount");
+	}
+
+
+	// @Before("forDaoPackage()")
+	@Before("forDaoPackageNoGetterSetter()")
+	public void performApiAnalytics() {
+		System.out.println("\n=======>>>>> Performing API analytics");
+	}
+}
